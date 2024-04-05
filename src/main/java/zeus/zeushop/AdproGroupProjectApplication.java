08caa62ef942eca281cdf8aaa3167a5f85f54911
goosetea04 <gusti.fatu@gmail.com>
2024-04-05 09:38:03 +0700

    Push Initial Sprng Boot files

---
+++ src/main/java/zeus/zeushop/AdproGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package zeus.zeushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdproGroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdproGroupProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zeus/zeushop/AdproGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package zeus.zeushop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdproGroupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
