9c39c6d950f00a223585b4c16c009bbd2e5bca09
goosetea04 <gusti.fatu@gmail.com>
2024-04-05 10:32:06 +0700

    Created model for listings

---
+++ src/main/java/zeus/zeushop/model/Listing.java
@@ -0,0 +1,10 @@
+package zeus.zeushop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+public class Listing {
+    private String productID;
+    private String productName;
+    private int productQuantity;
+    private int productPrice;
+}
