1a0e3745ffd55140d0303af14daac0cfce646730
adhis <tiva.adhisti@ui.ac.id>
2024-04-06 22:44:47 +0700

    created model for top up

---
+++ src/main/java/zeus/zeushop/model/TopUp.java
@@ -0,0 +1,15 @@
+package zeus.zeushop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class TopUp {
+    private String topUpId;
+    private String userId;
+    private double amount;
+    private String status; // for values like PENDING, APPROVED, CANCELLED
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
