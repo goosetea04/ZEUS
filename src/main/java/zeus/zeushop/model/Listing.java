574a1cee62bc230be31ebd2efe49cb49a4ef7dc9
goosetea04 <gusti.fatu@gmail.com>
2024-04-17 14:13:00 +0700

    [REFACTOR] added sellerid for listing model

--- src/main/java/zeus/zeushop/model/Listing.java
+++ src/main/java/zeus/zeushop/model/Listing.java
@@ -7,6 +7,7 @@ public class Listing {
     private String listingID;
     private String listingName;
     private String listingDescription;
+    private String SellerID;
     private int listingStock;
 
     private int listingPrice;
