eec588bad746260da7cdbe086986c9f6678cd00e
goosetea04 <gusti.fatu@gmail.com>
2024-04-20 20:07:06 +0700

    made listingreposiotry have JPA

--- src/main/java/zeus/zeushop/AdproGroupProjectApplication.java
+++ src/main/java/zeus/zeushop/AdproGroupProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdproGroupProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AdproGroupProjectApplication.class, args);
     }
-
 }
