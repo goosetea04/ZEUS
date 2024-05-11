50ba4824717e0cf954403fd5c1a1d8f07f999f38
cl4ytn <c.nagle744@gmail.com>
2024-05-11 10:01:57 +0700

    test: update user service

--- src/test/java/zeus/zeushop/service/UserServiceTest.java
+++ src/test/java/zeus/zeushop/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package zeus.zeushop.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import zeus.zeushop.model.User;
 import zeus.zeushop.repository.UserRepository;
 
@@ -11,22 +12,27 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 @SpringBootTest
 public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserServiceImpl userService;
 
-    /*
+
     @Test
     public void testRegisterUserSuccess() {
         User user = new User();
         user.setUsername("dummy");
         user.setPassword("asdf");
 
+        when(passwordEncoder.encode(any(String.class))).thenReturn(user.getPassword());
         when(userRepository.save(any(User.class))).thenReturn(user);
 
         User registeredUser = userService.createUser(user);
@@ -37,7 +43,7 @@ public class UserServiceTest {
         verify(userRepository, times(1)).save(any(User.class));
     }
 
-   
+
     @Test
     public void testRegisterUserFailure() {
         User user = new User();
@@ -49,5 +55,28 @@ public class UserServiceTest {
         assertNull(registeredUser);
         verify(userRepository, never()).save(any(User.class));
     }
-    */
+
+    @Test
+    public void testUpdateUser() {
+        User existingUser = new User();
+        existingUser.setId(1);
+        existingUser.setUsername("oldUsername");
+
+        User updatedUser = new User();
+        updatedUser.setId(1);
+        updatedUser.setUsername("newUsername");
+
+        when(userRepository.findById(eq(1))).thenReturn(Optional.of(existingUser));
+
+        when(userRepository.save(any(User.class))).thenReturn(updatedUser);
+
+        User resultUser = userService.updateUser(1, updatedUser);
+
+        assertNotNull(resultUser);
+        assertEquals("newUsername", resultUser.getUsername());
+
+        verify(userRepository, times(1)).findById(eq(1));
+
+        verify(userRepository, times(1)).save(any(User.class));
+    }
 }
