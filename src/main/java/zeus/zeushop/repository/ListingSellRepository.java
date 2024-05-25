a948353be1addcd81d93a97a757bcf9d0629382c
raisaafadilla <raisa.fadilla@ui.ac.id>
2024-05-26 03:47:09 +0700

    fix: solve conflicts

--- src/main/java/zeus/zeushop/repository/ListingSellRepository.java
+++ src/main/java/zeus/zeushop/repository/ListingSellRepository.java
@@ -13,9 +13,34 @@ import java.time.LocalDateTime;
 public interface ListingSellRepository extends JpaRepository<ListingSell, Integer> {
     // No need to implement these methods manually, JpaRepository provides them
 
+<<<<<<< HEAD
     // Custom method to find listings by a specific attribute, for example:
     // List<Listing> findByCategory(String category);
     List<ListingSell> findByEndDateGreaterThanEqual(LocalDateTime endDate); //ok thanks
     List<ListingSell> findByVisibleIsTrue();
     List<ListingSell> findBySellerId(Integer sellerId);
+=======
+    public ListingSell create(ListingSell listing) {
+        listingData.add(listing);
+        return listing;
+    }
+
+    public Iterator<ListingSell> findAll() {
+        return listingData.iterator();
+    }
+
+    public void deleteListingSell(ListingSell listingSell) {
+        listingData.remove(listingSell);
+    }
+
+    public void editListingSell(ListingSell listingSell) {
+        for (int i=0; i < listingData.size(); i++) {
+            ListingSell addListing = listingData.get(i);
+            if (addListing.getId().equals(listingSell.getId())) {
+                listingData.set(i, listingSell);
+                return;
+            }
+        }
+    }
+>>>>>>> aa32d76 (add delete and edit in ListingSellRepository)
 }
